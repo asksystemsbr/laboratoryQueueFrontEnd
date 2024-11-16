f64e4c9771f77b26d4626476c052994836cac9e2
alex_kaam <contato@asksystems.com.br>
2024-11-16 00:06:51 -0300

    -Initial

---
+++ src/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  rules: {
+    // Customize as regras conforme necessário
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+};
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <h1>Home Page</h1>
+      <p>Bem-vindo à página inicial!</p>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  
+  export default defineComponent({
+    name: 'Home',
+  });
+  </script>
+  
+  <style scoped>
+  /* Estilos específicos para o componente Home */
+  h1 {
+    color: #42b983;
+  }
+  </style>
+  
